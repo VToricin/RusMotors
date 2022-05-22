5954c2a9574c63445120fe989c79f11149d56b65
whiitered <whiitered@gmail.com>
2022-05-22 10:20:15 +0300

    redesign

---
+++ index.js
@@ -0,0 +1,10 @@
+let tmzButton = document.querySelector('.sectionYaMZ');
+let sectionYaMZ = document.querySelector('.sectionYaMZ');
+let sectionTMZ = document.querySelector('.sectionTMZ');
+function srollBottom (){
+    sectionTMZ.style.transform.scale
+}
+
+tmzButton.addEventListener('click', function(){
+
+})
